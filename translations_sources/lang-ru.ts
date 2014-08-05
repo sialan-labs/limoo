a9692e36c3fb87e4a83cdd8401a8468ad83b5edc
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2014-08-05 23:03:36 +0430

    Russian translation (Thanks freeniki)

---
+++ translations_sources/lang-ru.ts
@@ -0,0 +1,244 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>HomePage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <source>Donate us (Sialan Labs)</source>
+        <translation>Поддержать Sialan Labs</translation>
+    </message>
+    <message>
+        <source>Limoo</source>
+        <translation>Limoo</translation>
+    </message>
+</context>
+<context>
+    <name>AboutSialan</name>
+    <message>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <source>Sialan Labs website</source>
+        <translation>Сайт Sialan Labs</translation>
+    </message>
+    <message>
+        <source>Sialan Labs twitter</source>
+        <translation>Твиттер Sialan Labs</translation>
+    </message>
+</context>
+<context>
+    <name>Basket</name>
+    <message>
+        <source>Actions</source>
+        <translation>Действия</translation>
+    </message>
+</context>
+<context>
+    <name>BasketActions</name>
+    <message>
+        <source>Clear Basket</source>
+        <translation>Очистить корзину</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Открыть в</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+</context>
+<context>
+    <name>Configure</name>
+    <message>
+        <source>Configure</source>
+        <translation>Настройка</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <source>Thumbnalbar</source>
+        <translation>Панель миниатюр</translation>
+    </message>
+    <message>
+        <source>Fullscreen Thumbnalbar</source>
+        <translation>Полноэкранные миниатюры</translation>
+    </message>
+    <message>
+        <source>High Contrast</source>
+        <translation>Высокий контраст</translation>
+    </message>
+    <message>
+        <source>High Gamma</source>
+        <translation>Высокая гамма</translation>
+    </message>
+    <message>
+        <source>High Brightness</source>
+        <translation>Высокая яркость</translation>
+    </message>
+</context>
+<context>
+    <name>CreateFolderDialog</name>
+    <message>
+        <source>Create new folder</source>
+        <translation>Создать каталог</translation>
+    </message>
+    <message>
+        <source>Enter new folder name</source>
+        <translation>Ввести имя каталога</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Создать</translation>
+    </message>
+</context>
+<context>
+    <name>EditView</name>
+    <message>
+        <source>Select Application</source>
+        <translation>Выберите приложение</translation>
+    </message>
+</context>
+<context>
+    <name>ImageDeleteDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+</context>
+<context>
+    <name>ImageMenu</name>
+    <message>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Открыть в</translation>
+    </message>
+    <message>
+        <source>Background</source>
+        <translation>Фон рабочего стола</translation>
+    </message>
+    <message>
+        <source>Set Cover</source>
+        <translation>Как обложка</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Детали</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>New folder</source>
+        <translation>Новый каталог</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+</context>
+<context>
+    <name>Limoo</name>
+    <message>
+        <source>Sialan Labs is a not-for-profit research and software development team launched in February 2014 focusing on development of products, technologies and solutions in order to publish them as open-source projects accessible to all people in the universe. Currently, we are focusing on design and development of software applications and tools which have direct connection with end users.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>By enabling innovative projects and distributing software to millions of users globally, the lab is working to accelerate the growth of high-impact open source software projects and promote an open source culture of accessibility and increased productivity around the world. The lab partners with industry leaders and policy makers to bring open source technologies to new sectors, including education, health and government.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Limoo is a modern image viewer ,focusing on user interface and user friendly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Limoo is Free software and released under GPLv3 License.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename Dialog</source>
+        <translation>Перевести диалог</translation>
+    </message>
+    <message>
+        <source>Enter new name</source>
+        <translation>Ввести новое имя</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Переименовать</translation>
+    </message>
+</context>
+<context>
+    <name>ToolBar</name>
+    <message>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Открыть в</translation>
+    </message>
+    <message>
+        <source>Rotate Left</source>
+        <translation>Повернуть влево</translation>
+    </message>
+    <message>
+        <source>Rotate Right</source>
+        <translation>Повернуть вправо</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>Поный экран</translation>
+    </message>
+    <message>
+        <source>Limoo</source>
+        <translation>Limoo</translation>
+    </message>
+    <message>
+        <source>Sialan</source>
+        <translation>Sialan</translation>
+    </message>
+    <message>
+        <source>Configure</source>
+        <translation>Настройка</translation>
+    </message>
+</context>
+</TS> 

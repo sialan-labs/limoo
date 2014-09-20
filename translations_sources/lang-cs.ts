f0c0734f450a91261540faacc7cd013ad0f75d13
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2014-09-20 10:51:38 +0430

    Add czech translations (Thanks Pavel)

---
+++ translations_sources/lang-cs.ts
@@ -0,0 +1,244 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>HomePage</source>
+        <translation>Stránky</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Zpět</translation>
+    </message>
+    <message>
+        <source>Donate us (Sialan Labs)</source>
+        <translation>Darovat (Sialan Labs)</translation>
+    </message>
+    <message>
+        <source>Limoo</source>
+        <translation>Limoo</translation>
+    </message>
+</context>
+<context>
+    <name>AboutSialan</name>
+    <message>
+        <source>Back</source>
+        <translation>Zpět</translation>
+    </message>
+    <message>
+        <source>Sialan Labs website</source>
+        <translation>Stránky Sialan Labs</translation>
+    </message>
+    <message>
+        <source>Sialan Labs twitter</source>
+        <translation>Twitter Sialan Labs</translation>
+    </message>
+</context>
+<context>
+    <name>Basket</name>
+    <message>
+        <source>Actions</source>
+        <translation>Činnosti</translation>
+    </message>
+</context>
+<context>
+    <name>BasketActions</name>
+    <message>
+        <source>Clear Basket</source>
+        <translation>Vyprázdnit koš</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Otevřít pomocí</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Vyjmout</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopírovat</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Smazat</translation>
+    </message>
+</context>
+<context>
+    <name>Configure</name>
+    <message>
+        <source>Configure</source>
+        <translation>Nastavit</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Jazyk</translation>
+    </message>
+    <message>
+        <source>Thumbnalbar</source>
+        <translation>Pruh s náhledy</translation>
+    </message>
+    <message>
+        <source>Fullscreen Thumbnalbar</source>
+        <translation>Pruh s náhledy v režimu na celou obrazovku</translation>
+    </message>
+    <message>
+        <source>High Contrast</source>
+        <translation>Vysoký kontrast</translation>
+    </message>
+    <message>
+        <source>High Gamma</source>
+        <translation>Vysoká gamma</translation>
+    </message>
+    <message>
+        <source>High Brightness</source>
+        <translation>Vysoký jas</translation>
+    </message>
+</context>
+<context>
+    <name>CreateFolderDialog</name>
+    <message>
+        <source>Create new folder</source>
+        <translation>Vytvořit novou složku</translation>
+    </message>
+    <message>
+        <source>Enter new folder name</source>
+        <translation>Zadejte název nové složky</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Vytvořit</translation>
+    </message>
+</context>
+<context>
+    <name>EditView</name>
+    <message>
+        <source>Select Application</source>
+        <translation>Vybrat aplikaci</translation>
+    </message>
+</context>
+<context>
+    <name>ImageDeleteDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+</context>
+<context>
+    <name>ImageMenu</name>
+    <message>
+        <source>Cut</source>
+        <translation>Vyjmout</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopírovat</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Vložit</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Otevřít</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Otevřít pomocí</translation>
+    </message>
+    <message>
+        <source>Background</source>
+        <translation>Pozadí</translation>
+    </message>
+    <message>
+        <source>Set Cover</source>
+        <translation>Nastavit obal</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Podrobnosti</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Smazat</translation>
+    </message>
+    <message>
+        <source>New folder</source>
+        <translation>Nová složka</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Přejmenovat</translation>
+    </message>
+</context>
+<context>
+    <name>Limoo</name>
+    <message>
+        <source>Sialan Labs is a not-for-profit research and software development team launched in February 2014 focusing on development of products, technologies and solutions in order to publish them as open-source projects accessible to all people in the universe. Currently, we are focusing on design and development of software applications and tools which have direct connection with end users.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>By enabling innovative projects and distributing software to millions of users globally, the lab is working to accelerate the growth of high-impact open source software projects and promote an open source culture of accessibility and increased productivity around the world. The lab partners with industry leaders and policy makers to bring open source technologies to new sectors, including education, health and government.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Limoo is a modern image viewer ,focusing on user interface and user friendly.</source>
+        <translation>Limoo je pokrokový prohlížeč obrázků. Zaměřuje se na uživatelské rozhraní a uživatelskou přívětivost.</translation>
+    </message>
+    <message>
+        <source>Limoo is Free software and released under GPLv3 License.</source>
+        <translation>Limoo je svobodný program vydaný pod licencí GPLv3.</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <source>Rename Dialog</source>
+        <translation>Dialog pro přejmenování</translation>
+    </message>
+    <message>
+        <source>Enter new name</source>
+        <translation>Zadejte nový název</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Přejmenovat</translation>
+    </message>
+</context>
+<context>
+    <name>ToolBar</name>
+    <message>
+        <source>Back</source>
+        <translation>Zpět</translation>
+    </message>
+    <message>
+        <source>Open With</source>
+        <translation>Otevřít pomocí</translation>
+    </message>
+    <message>
+        <source>Rotate Left</source>
+        <translation>Otočit vlevo</translation>
+    </message>
+    <message>
+        <source>Rotate Right</source>
+        <translation>Otočit vpravo</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>Režim na celou obrazovku</translation>
+    </message>
+    <message>
+        <source>Limoo</source>
+        <translation>Limoo</translation>
+    </message>
+    <message>
+        <source>Sialan</source>
+        <translation>Sialan</translation>
+    </message>
+    <message>
+        <source>Configure</source>
+        <translation>Nastavit</translation>
+    </message>
+</context>
+</TS>
